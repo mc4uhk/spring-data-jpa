4088894612298b529b58a8376ba71405e42b8e72
Edmund Yau <mc4u.hk@gmail.com>
2021-03-31 22:32:40 +0800

    Added stream example

---
+++ src/test/java/test/Test06.java
@@ -0,0 +1,47 @@
+package test;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test06 {
+	private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	@Test
+	public void test01() {
+		List<Map<String, String>> mapList = new ArrayList<>();
+		for (int i = 0; i < 100; i++) {
+			Map<String, String> map1 = new HashMap<>();
+			Random rand = new Random();
+			map1.put("value", String.valueOf(rand.nextInt(100)));
+			mapList.add(map1);
+		}
+
+			mapList.stream()
+				.map(this::colorize)
+				.map(m -> m.get("colorCode"))
+				.forEach(log::info);
+
+	}
+
+	public Map<String, String> colorize(Map<String, String> m) {
+		Integer value = Integer.valueOf(m.get("value"));
+		if (value < 30)
+			m.put("colorCode", "R");
+		else if (30 <= value && value < 70)
+			m.put("colorCode", "Y");
+		else
+			m.put("colorCode", "G");
+
+		return m;
+	}
+
+}
